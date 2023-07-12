0f338d57f0ee9d6536c47a7056bf620fbf2868c2
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-12 21:27:44 +0200

    Update app.py

--- FinalPr/app.py
+++ FinalPr/app.py
@@ -6,8 +6,28 @@ from pages.recomend.plots import plots
 #from pages.scrap import home
 
 
+#def main():
+#    # Configurar la barra lateral para seleccionar la página
+#    st.sidebar.title('Selector de Páginas')
+#    page = st.sidebar.selectbox('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping'])
+
+ #   # Dependiendo de la página seleccionada, mostrar el contenido correspondiente
+#    if page == 'Home':
+#        home.main()
+#    elif page == 'Detección':
+ #       detection.main()
+#   elif page == 'Recomendación':
+ #       recomend.main()
+  #  elif page == 'Plots':
+  #      plots.main()
+ #  elif page == 'Scraping':
+  #      home.main()
+
+#if __name__ == "__main__":
+#    main()
+
 def main():
-    # Configurar la barra lateral para seleccionar la página
+    # Configurando la barra lateral para seleccionar la página
     st.sidebar.title('Selector de Páginas')
     page = st.sidebar.selectbox('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping'])
 
@@ -17,9 +37,12 @@ def main():
     elif page == 'Detección':
         detection.main()
     elif page == 'Recomendación':
-        recomend.main()
-    elif page == 'Plots':
-        plots.main()
+        st.sidebar.title('Selector de Recomendación') # Nuevo título para la sub-selección
+        recomend_option = st.sidebar.selectbox('Seleccione una opción', options=['Código', 'Gráficos']) # Nuevo selectbox para sub-selección
+        if recomend_option == 'Código':
+            recomend.main()
+        elif recomend_option == 'Gráficos':
+            plots.main() # Ejecuta esto si 'Gráficos' es seleccionado
     elif page == 'Scraping':
         home.main()
 
