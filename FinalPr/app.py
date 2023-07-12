713e72aac6051c64121ad9cfcc17eee8f4ca939b
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-12 17:46:09 +0200

    Update app.py

--- FinalPr/app.py
+++ FinalPr/app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import squarify
+from PIL import Image
+from wordcloud import WordCloud
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pdimport streamlit as st
+import pandas as pd
+import plots
 import home
 from pages.detection import detection
 from pages.recomend import recomend
