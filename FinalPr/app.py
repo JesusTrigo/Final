e55576be6c228562953fc9bfb37215bc1c102a4e
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-13 01:25:43 +0200

    Update app.py

--- FinalPr/app.py
+++ FinalPr/app.py
@@ -2,13 +2,32 @@ import streamlit as st
 import home
 from pages.detection import detection
 from pages.recomend import recomend
-from pages.recomend.plots import plots # Importa main de plots.py como plots
+from pages.recomend.plots import plots
 #from pages.scrap import home
 
-def main():
+#def main():
     # Configurar la barra lateral para seleccionar la página
+    #st.sidebar.title('Selector de Páginas')
+    #page = st.sidebar.selectbox('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping'])
+
+    # Dependiendo de la página seleccionada, mostrar el contenido correspondiente
+    #if page == 'Home':
+     #   home.main()
+    #elif page == 'Detección':
+     #   detection.main()
+    #elif page == 'Recomendación':
+    #    recomend.main() # Esta función ahora manejará tanto recomend como plots
+   #elif page == 'Scraping':
+   #     home.main()
+
+#if __name__ == "__main__":
+ #  main()
+
+
+def main():
+    # Configurando la barra lateral para seleccionar la página
     st.sidebar.title('Selector de Páginas')
-    page = st.sidebar.selectbox('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping'])
+    page = st.sidebar.selectbox('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping', 'Plots'])
 
     # Dependiendo de la página seleccionada, mostrar el contenido correspondiente
     if page == 'Home':
@@ -16,33 +35,14 @@ def main():
     elif page == 'Detección':
         detection.main()
     elif page == 'Recomendación':
-        recomend.main() # Esta función ahora manejará tanto recomend como plots
+        st.sidebar.title('Selector de Recomendación') # Nuevo título para la sub-selección
+        recomend_option = st.sidebar.selectbox('Seleccione una opción', options=['Código', 'Gráficos']) # Nuevo selectbox para sub-selección
+        if recomend_option == 'Código':
+            recomend.main()
+        elif recomend_option == 'Gráficos':
+            plots.main() # Llama a plots(), que se refiere a main() en plots.py
     elif page == 'Scraping':
         home.main()
 
 if __name__ == "__main__":
     main()
-
-
-#def main():
-    # Configurando la barra lateral para seleccionar la página
-    #st.sidebar.title('Selector de Páginas')
-    #Ω#page = st.sidebar.selectbox('Seleccione una página', options=['Home', 'Detección', 'Recomendación', 'Scraping'])
-
-    # Dependiendo de la página seleccionada, mostrar el contenido correspondiente
-    #if page == 'Home':
-     #   home.main()
-    #elif page == 'Detección':
-    #    detection.main()
-   # elif page == 'Recomendación':
-  #      st.sidebar.title('Selector de Recomendación') # Nuevo título para la sub-selección
- #       recomend_option = st.sidebar.selectbox('Seleccione una opción', options=['Código', 'Gráficos']) # Nuevo selectbox para sub-selección
-#        if recomend_option == 'Código':
-  #          recomend.main()
- #       elif recomend_option == 'Gráficos':
- #           plots.main() # Llama a plots(), que se refiere a main() en plots.py
- #   elif page == 'Scraping':
- #       home.main()
-
-#if __name__ == "__main__":
-   # main()
