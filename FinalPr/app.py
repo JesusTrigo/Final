e35c3b43db17b57c4eb5b63c80d93b1066f09c1d
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-12 17:48:15 +0200

    Update app.py

--- FinalPr/app.py
+++ FinalPr/app.py
@@ -1,4 +1,10 @@
-pip install -r requirements.txt
+import subprocess
+import sys
+
+def install_requirements():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
+install_requirements()
 import streamlit as st
 import cv2
 import numpy as np
