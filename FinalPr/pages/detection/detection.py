e15ad17d41b717acd2ff4b19251ec3ba3f68a675
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-13 13:50:43 +0200

    Update detection.py

--- FinalPr/pages/detection/detection.py
+++ FinalPr/pages/detection/detection.py
@@ -30,8 +30,24 @@ def main():
     st.title("Detector de objetos a través de imágenes")
 
     # Mostrar las clases de objetos con las que el algoritmo está entrenado en columnas
-
-    st.header("El algoritmo está entrenado con las siguientes imágenes:")
+    st.write("""
+    Este proyecto es una aplicación web dinámica e interactiva de ciencia de datos que utiliza algoritmos de aprendizaje automático profundo para identificar y clasificar imágenes 
+    
+    introducidas por los usuarios. Su objetivo principal es proporcionar a los usuarios la capacidad de analizar y clasificar imágenes con precisión y eficiencia, 
+    
+    ya sea para propósitos académicos, de investigación, o para cualquier uso práctico.
+    
+    Nuestro enfoque consistió en adaptar y personalizar un algoritmo de red neuronal convolucional preentrenado. 
+    
+    Este algoritmo es conocido por su eficiencia en el procesamiento y reconocimiento de imágenes debido a su habilidad para detectar y aprender patrones 
+    
+    en los datos de entrada.En lugar de entrenar un modelo desde cero, que puede requerir una gran cantidad de tiempo y recursos, optamos por utilizar técnicas de transferencia de aprendizaje.
+    
+    Esto implica tomar un modelo preentrenado y adaptarlo a nuestras necesidades específicas, lo que nos permite beneficiarnos del aprendizaje previo del modelo y ahorrar en recursos de entrenamiento.
+    """)
+    
+    st.write("El algoritmo está entrenado con las siguientes imágenes
+    "")
     col1, col2, col3 = st.columns(3)
     col1.header("")
     col2.header("")
