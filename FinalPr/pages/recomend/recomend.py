a53d7cfba05f73e6bad2a7e2f4212c25486d2d9a
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-13 03:09:22 +0200

    Update recomend.py

--- FinalPr/pages/recomend/recomend.py
+++ FinalPr/pages/recomend/recomend.py
@@ -1,25 +1,32 @@
+# Importación de las librerías necesarias
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import subprocess
-#from . import plots
+import plots
+import random
 
+# Desactivar advertencias de desaprobación de Streamlit
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-@st.cache_data
+# Función para cargar los datos desde una URL. Esta función está decorada con st.cache, 
+# lo que significa que los datos solo se descargarán la primera vez que se ejecute la aplicación. 
+# En las ejecuciones subsiguientes, se cargará una versión en caché de los datos.
+@st.cache
 def read_csv_streamlit():
     url = "https://drive.google.com/u/0/uc?id=1ePhuTPZWNkW4Nw634dXxV21fneJRgNWo&export=download&confirm=t&uuid=61491d58-19cc-11ee-be56-0242ac120002"
     df = pd.read_csv(url)
     return df
 
 def main():
-    url = "https://drive.google.com/u/0/uc?id=1ePhuTPZWNkW4Nw634dXxV21fneJRgNWo&export=download&confirm=t&uuid=61491d58-19cc-11ee-be56-0242ac120002"
+    # Cargamos los datos de la cerveza
     df_Beer = read_csv_streamlit()
-    df_display = df_Beer.head()
 
+    # Mostramos un avance de los datos en la interfaz de usuario
+    df_display = df_Beer.head()
     st.subheader("Vista previa del DataFrame:")
     st.write(df_display)
 
+    # Creamos una tabla pivote de los datos
     matriz_df = df_Beer.pivot_table(
         values='review/overall',
         index='review/profileName',
@@ -27,39 +34,42 @@ def main():
         aggfunc='max'
     )
 
+    # Obtenemos una lista de todas las cervezas
     all_beers = matriz_df.columns.tolist()
 
+    # Imprimimos mensajes en la interfaz de usuario
     st.title("¡Hola! Vamos a recomendarte una cerveza según tus gustos previos...")
     st.write(' ')
     st.subheader("El proceso demora unos segundos debido a la cantidad de datos, gracias por su paciencia")
     st.write(' ')
 
-    with st.form(key='my_form'):
-        beer_choice1 = st.selectbox('Selecciona tu primer estilo de cerveza:', all_beers, key="beer1")
-        rating1 = st.slider('Califica el primer estilo de cerveza:', 0.0, 5.0, step=0.5, key="rating1")
+    # Preparamos las opciones para la selección de cerveza
+    beer_choices = ["Selecciona una cerveza..."] + all_beers
 
-        beer_choice2 = st.selectbox('Selecciona tu segundo estilo de cerveza:', [beer for beer in all_beers if beer != beer_choice1], key="beer2")
-        rating2 = st.slider('Califica el segundo estilo de cerveza:', 0.0, 5.0, step=0.5, key="rating2")
+    # Preparamos un diccionario vacío para almacenar las calificaciones del usuario
+    user_ratings = {}
 
-        beer_choice3 = st.selectbox('Selecciona tu tercer estilo de cerveza:', [beer for beer in all_beers if beer != beer_choice1 and beer != beer_choice2], key="beer3")
-        rating3 = st.slider('Califica el tercer estilo de cerveza:', 0.0, 5.0, step=0.5, key="rating3")
+    # Solicitamos al usuario que seleccione y califique 5 cervezas
+    for i in range(1, 6):
+        beer_choice = st.selectbox(f'Selecciona tu {i}º estilo de cerveza:', beer_choices, key=f"beer{i}")
+        rating = st.slider(f'Califica el {i}º estilo de cerveza:', 0.0, 5.0, step=0.5, key=f"rating{i}")
 
-        submit_button = st.form_submit_button(label='Submit')
+        # Solo guardamos las calificaciones de las cervezas que el usuario realmente seleccionó
+        if beer_choice != "Selecciona una cerveza...":
+            user_ratings[beer_choice] = rating
 
+    # Proporcionamos un botón para enviar las calificaciones
+    submit_button = st.button('Submit')
+
+    # Si el usuario presiona el botón de enviar, realizamos el cálculo de las recomendaciones
     if submit_button:
-        st.session_state.new_user_ratings = {
-            beer_choice1: rating1,
-            beer_choice2: rating2,
-            beer_choice3: rating3
-        }
-
         st.subheader("Los estilos seleccionados y sus calificaciones son:")
-        st.write(st.session_state.new_user_ratings)
+        st.write(user_ratings)
 
-        nuevo_usuario_serie = pd.Series(st.session_state.new_user_ratings, name='Nuevo Usuario')
+        nuevo_usuario_serie = pd.Series(user_ratings, name='Nuevo Usuario')
         matriz_df.loc['Nuevo Usuario'] = nuevo_usuario_serie
 
-        cervezas_calificadas = list(st.session_state.new_user_ratings.keys())
+        cervezas_calificadas = list(user_ratings.keys())
         usuarios_similares = matriz_df.dropna(subset=cervezas_calificadas)
         usuarios_similares = usuarios_similares.apply(pd.to_numeric, errors='coerce')
 
@@ -78,31 +88,38 @@ def main():
 
         predicted_ratings = weighted_rating_matrix_sum / similarity_sum
 
-        recommended_beer_indices = np.argsort(-predicted_ratings)[:5]
-        recommended_beers = matriz_df.columns[recommended_beer_indices]
+        # Obtenemos los índices de las 5 cervezas con mayor puntuación predicha
+        top_5_beer_indices = np.argsort(-predicted_ratings)[:5]
 
-        recommended_style = recommended_beers[0]
+        # Seleccionamos al azar un índice entre los 5 principales. 
+        # Esto introduce variedad en las recomendaciones, en lugar de simplemente seleccionar la cerveza con mayor puntuación cada vez.
+        recommended_beer_index = random.choice(top_5_beer_indices)
 
-        beers_of_recommended_style = df_Beer[df_Beer['beer/style'] == recommended_style]
-        highest_rated_beer = beers_of_recommended_style.loc[beers_of_recommended_style['review/overall'].idxmax()]['beer/name']
+        # Obtenemos el nombre de la cerveza recomendada
+        recommended_beer = matriz_df.columns[recommended_beer_index]
 
+        # Obtenemos todas las cervezas del estilo recomendado
+        beers_of_recommended_style = df_Beer[df_Beer['beer/style'] == recommended_beer]
+
+        # Obtenemos las cervezas de mayor puntuación dentro del estilo recomendado
+        highest_rated_beers = beers_of_recommended_style[beers_of_recommended_style['review/overall'] == beers_of_recommended_style['review/overall'].max()]
+
+        # Seleccionamos al azar una cerveza entre las de mayor puntuación. 
+        # Esto introduce variedad en las recomendaciones, en lugar de simplemente seleccionar la cerveza de mayor puntuación cada vez.
+        highest_rated_beer = random.choice(highest_rated_beers['beer/name'].tolist())
+
+        # Imprimimos la recomendación en la interfaz de usuario
         st.markdown(
-            f'Según tus preferencias, la recomendación por _Estilo de Cerveza_ que debes probar es:<br><b>{recommended_style}</b>, '
+            f'Según tus preferencias, la recomendación por _Estilo de Cerveza_ que debes probar es:<br><b>{recommended_beer}</b> '
             f'<br><br>La _Cerveza_ recomendada es:<br><b>{highest_rated_beer}</b>.',
+Aquí está la continuación del código anterior (el código era demasiado largo para caber en una sola respuesta):
+
+```python
             unsafe_allow_html=True
         )
-        
+
+    # Mostramos un gráfico de las cervezas más comunes
+    plots.plot_most_common_beer_bar(df_Beer)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
