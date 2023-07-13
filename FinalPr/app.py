0b100754f9336483b746af88ff658b1746e8eb6d
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-13 23:44:23 +0200

    Update app.py

--- FinalPr/app.py
+++ FinalPr/app.py
@@ -33,7 +33,6 @@ def main():
             plots.plot_abv_beer_style_box(df_Beer)
             plots.plot_3d_scatter_overall_palate_taste(df_Beer)
             plots.plot_beer_wordcloud(df_Beer)
-            plots.plot_3d_mesh(df_Beer)
     elif page == 'Scraping':
         scrap.main()
 
