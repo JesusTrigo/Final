54270e15fdf702a1f28f200668dc368002bd2188
JesúsT <83574910+JesusTrigo@users.noreply.github.com>
2023-07-13 02:45:47 +0200

    Update plots.py

--- FinalPr/pages/recomend/plots/plots.py
+++ FinalPr/pages/recomend/plots/plots.py
@@ -59,11 +59,11 @@ def main():
     
     # Función para crear una nube de palabras con los nombres de las cervezas
     
-    def plot_beer_wordcloud(df_Beer, img_path):
+    def plot_beer_wordcloud(df_Beer):
         # Contar cuántas veces aparece cada nombre de cerveza
         beer_counts = df_Beer['beer/name'].value_counts()
         # Cargar una máscara con la forma de una cerveza
-        beer_mask = np.array(Image.open(img_path))
+        beer_mask = np.array(Image.open('/app/final/FinalPr/pages/recomend/images/beer.png'))
         # Crear una nube de palabras con la máscara de la cerveza y otras configuraciones
         wordcloud = WordCloud(width=800, height=400, background_color='white', max_words=100, mask=beer_mask, contour_width=3, contour_color='black')
         # Generar la nube de palabras a partir de las frecuencias de los nombres de las cervezas
